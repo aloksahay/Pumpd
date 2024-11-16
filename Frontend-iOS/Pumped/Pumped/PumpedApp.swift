87c80182da590a7327fee0c03d74bdc845300500
Alok Sahay <alok.sahay87@gmail.com>
2024-11-16 16:12:02 +0700

    Add frontend

---
+++ Frontend-iOS/Pumped/Pumped/PumpedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PumpedApp.swift
+//  Pumped
+//
+//  Created by Alok Sahay on 16.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct PumpedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
